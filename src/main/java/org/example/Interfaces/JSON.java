0e464c18a934d88d52e0500be9e0d1929b5bf1b3
rivatoDev <tomasrivara23.7@gmail.com>
2024-10-28 22:58:08 -0300

    Creacion de Tren y Pasajero

--- src/main/java/org/example/Interfaces/JSON.java
+++ src/main/java/org/example/Interfaces/JSON.java
@@ -6,5 +6,9 @@ import org.json.JSONObject;
  * Metodos relacionados a JSON.
  */
 public interface JSON {
+    /**
+     * Convierte el objeto a un JSONObject.
+     * @return El objeto como un JSONObject.
+     */
     JSONObject convertirAJSONObject();
 }
